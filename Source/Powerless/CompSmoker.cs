5060137288079b75994c5cbdfea767df67de6319
cuproPanda <cupropanda@gmail.com>
2017-05-05 14:35:08 -0400

    Camp Tables and Cabbage Salad

--- Source/Powerless/CompSmoker.cs
+++ Source/Powerless/CompSmoker.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 using UnityEngine;
 using RimWorld;
 using Verse;
-using Verse.AI;
 
 namespace Powerless {
 
