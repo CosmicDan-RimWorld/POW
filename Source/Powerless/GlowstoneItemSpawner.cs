f3653cdd32e8164c22f719314e96a5a1e645d540
cuproPanda <cupropanda@gmail.com>
2017-05-30 19:52:28 -0400

    Added traders

--- Source/Powerless/GlowstoneItemSpawner.cs
+++ Source/Powerless/GlowstoneItemSpawner.cs
@@ -1,20 +1,24 @@
 ﻿using Verse;
 
 namespace Powerless {
-  
-  internal class Turnip_Spawner : Thing {
+
+  internal class GlowstoneItemSpawner : Thing {
 
 
     public override void SpawnSetup(Map map, bool respawningAfterLoad) {
       base.SpawnSetup(map, respawningAfterLoad);
-      SpawnRandomQuantity(ThingDef.Named("POW_Turnip"), 1, 2, map);
-      SpawnRandomQuantity(ThingDef.Named("POW_Turnip_Green"), 1, 4, map);
+      SpawnRandomQuantity(ThingDef.Named("POW_LargeGlowstone"), -4, 2, map);
+      SpawnRandomQuantity(ThingDef.Named("POW_SmallGlowstone"), 1, 8, map);
+      SpawnRandomQuantity(ThingDef.Named("POW_GlowstoneDust"), -5, 10, map);
       Destroy();
     }
 
 
     private void SpawnRandomQuantity(ThingDef tDef, int minToSpawn, int maxToSpawn, Map map) {
       int stack = Rand.RangeInclusive(minToSpawn, maxToSpawn);
+      if (stack <= 0) {
+        return;
+      }
       Thing placedProduct = ThingMaker.MakeThing(tDef);
       placedProduct.stackCount = stack;
       GenPlace.TryPlaceThing(placedProduct, Position, map, ThingPlaceMode.Near);
