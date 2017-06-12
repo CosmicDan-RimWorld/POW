4c4933565a23103ee310402eb6668f0797af5787
cuproPanda <cupropanda@gmail.com>
2017-06-12 18:57:42 -0400

    Fixed chandelier ticking

--- Source/Powerless/Components/CompChandelier.cs
+++ Source/Powerless/Components/CompChandelier.cs
@@ -6,32 +6,26 @@ namespace Powerless {
 
   public class CompChandelier : ThingComp {
 
-    private Map parentMap;
-
-
-    public override void PostSpawnSetup(bool respawningAfterLoad) {
-      base.PostSpawnSetup(respawningAfterLoad);
-
-      parentMap = parent.Map;
-    }
-
 
     public override void CompTickRare() {
       base.CompTickRare();
 
       // Minify this if the ceiling is missing
       int occCells = 0;
-      int roofCells = 0;
-      foreach (IntVec3 c in parent.OccupiedRect()) {
+      int unroofedCells = 0;
+      foreach (IntVec3 current in parent.OccupiedRect()) {
         occCells++;
-        if (!parentMap.roofGrid.Roofed(c)) {
-          roofCells++;
+        if (!parent.Map.roofGrid.Roofed(current)) {
+          unroofedCells++;
         }
-        if (c.GetEdifice(parent.Map).def.blockWind == true || c.GetEdifice(parent.Map).def.holdsRoof == true) {
+        if (current.GetEdifice(parent.Map) == null || current.GetEdifice(parent.Map).def == null) {
+          continue;
+        }
+        if (current.GetEdifice(parent.Map).def.blockWind == true || current.GetEdifice(parent.Map).def.holdsRoof == true) {
           Minify();
         }
       }
-      if (((float)(occCells - roofCells) / occCells) < 0.5f) {
+      if (((float)(occCells - unroofedCells) / occCells) < 0.5f) {
         Minify();
       }
     }
@@ -39,8 +33,8 @@ namespace Powerless {
 
     public virtual void Minify() {
       MinifiedThing package = parent.MakeMinified();
-      GenPlace.TryPlaceThing(package, parent.Position, parentMap, ThingPlaceMode.Near);
-      SoundDef.Named("ThingUninstalled").PlayOneShot(new TargetInfo(parent.Position, parentMap));
+      GenPlace.TryPlaceThing(package, parent.Position, parent.Map, ThingPlaceMode.Near);
+      SoundDef.Named("ThingUninstalled").PlayOneShot(new TargetInfo(parent.Position, parent.Map));
     }
   }
 }
